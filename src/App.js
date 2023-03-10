471942d1156a1ba2712d3b2133ecf167039248b1
Lily Ochs <70216903+lhochs@users.noreply.github.com>
2023-03-10 16:40:47 -0600

    fine tuning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
 import SignUp from './components/SignUp';
-// import OccupationApi from './components/OccupationApi'
-// import StatesApi from './components/StatesApi'
 import React from 'react';
 
 
@@ -9,8 +7,6 @@ function App() {
   return (
     <div className="App">
       <SignUp/>
-      {/* <OccupationApi/> */}
-      {/* <StatesApi/> */}
     </div>
   );
 }
